8dbd95c05b6e7d81c22e90284ac84e25cac0475d
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-23 19:52:02 -0300

    feat: create error message component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { ErroMessageComponent } from './components/shared/error-message/error-message.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, ErroMessageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
