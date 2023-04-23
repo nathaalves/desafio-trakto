0b0dd1d1a70826c3de24dadf2a0674bdebd5344c
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-23 19:56:16 -0300

    feat: create login page compopnent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
 import { ErroMessageComponent } from './components/shared/error-message/error-message.component';
 
 @NgModule({
-  declarations: [AppComponent, ErroMessageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, ErroMessageComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public isFetching = false;
+  public errorMessage: string | null = null;
+
+  onLoginRequest(credentials: { email: string; password: string }) {
+    this.isFetching = true;
+  }
+}
