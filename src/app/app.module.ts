427d9473b903b4056a5f1658affb0cd1ef4acf38
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-23 20:10:46 -0300

    feat: import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { ErroMessageComponent } from './components/shared/error-message/error-me
 
 @NgModule({
   declarations: [AppComponent, ErroMessageComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
