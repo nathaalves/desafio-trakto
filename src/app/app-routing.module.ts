7bdd9db11d244c0d7b4edfb58782a45c8a27f67c
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-25 19:56:33 -0300

    feat: add courseware component to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './guards/auth.guard';
+import { CousewareComponent } from './pages/couseware/couseware.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'material-didatico',
+    component: CousewareComponent,
     canActivate: [AuthGuard],
   },
   { path: 'login', component: LoginComponent },
