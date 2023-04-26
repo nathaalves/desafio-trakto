e32b6a53696b64b1db7c8872ea54ca940e57fd90
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-26 11:31:25 -0300

    fix: change file name

---
+++ src/app/pages/courseware/courseware.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, take, exhaustMap } from 'rxjs';
+import { Courseware } from './courseware.model';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursewareService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  fetchData() {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.http.get('https://api.trakto.io/document', {
+          headers: {
+            Authorization: `Bearer ${user?.token}`,
+          },
+        });
+      }),
+      map((res: any) => {
+        const courceware = [];
+        for (let data of res.data) {
+          courceware.push(
+            new Courseware(
+              data.id,
+              data.title,
+              data.thumbs.low,
+              data.pages.length,
+              data.updated_at
+            )
+          );
+        }
+        return {
+          data: courceware,
+          hasNextPage: res.hasNextPage,
+          hasPreviousPage: res.hasPreviousPage,
+        };
+      })
+    );
+  }
+}
