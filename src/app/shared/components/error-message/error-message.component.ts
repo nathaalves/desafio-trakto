dcb800c57e1d1821609da134911266a411a930b3
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-26 22:17:50 -0300

    feat: rename shared folder to components

--- src/app/shared/components/error-message/error-message.component.ts
+++ src/app/shared/components/error-message/error-message.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-error-message',
   templateUrl: './error-message.component.html',
-  styleUrls: ['./error-message.component.css'],
 })
 export class ErroMessageComponent {
   @Input() message: string | null = null;
