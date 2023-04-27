46dc09137f06f1b426993f05b1273de7e31bacb8
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-27 00:22:23 -0300

    refactor: split app and app-routing modules in feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './features/login/login.component';
-import { HomeComponent } from './features/home/home.component';
-import { AuthGuard } from './shared/guards/auth.guard';
-import { CoursewareComponent } from './features/courseware/courseware.component';
-import { CoursewareHomeComponent } from './features/courseware/pages/courseware-home/courseware-home.component';
-import { CoursewareSeeAllComponent } from './features/courseware/pages/courseware-see-all/courseware-see-all.component';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  {
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'material-didatico',
-    component: CoursewareComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: '', component: CoursewareHomeComponent },
-      { path: 'ver-todos', component: CoursewareSeeAllComponent },
-    ],
-  },
-  { path: 'login', component: LoginComponent },
-];
+const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'home' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { LoginModule } from './features/login/login.module';
+import { HomeModule } from './features/home/home.module';
+import { CoursewareModule } from './features/courseware/courseware.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './features/login/login.component';
-import { ErroMessageComponent } from './shared/components/error-message/error-message.component';
-import { HomeComponent } from './features/home/home.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { HomeAnchorComponent } from './features/home/components/home-anchor/home-anchor.component';
-import { CoursewareComponent } from './features/courseware/courseware.component';
-import { CourseComponent } from './features/courseware/components/course/course.component';
-import { CoursewareSectionComponent } from './features/courseware/components/courseware-section/courseware-section.component';
-import { CoursewareHomeComponent } from './features/courseware/pages/courseware-home/courseware-home.component';
-import { CoursewareSeeAllComponent } from './features/courseware/pages/courseware-see-all/courseware-see-all.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ErroMessageComponent,
-    LoginComponent,
-    HomeComponent,
-    HeaderComponent,
-    HomeAnchorComponent,
-    CoursewareComponent,
-    CourseComponent,
-    CoursewareSectionComponent,
-    CoursewareHomeComponent,
-    CoursewareSeeAllComponent,
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    LoginModule,
+    HomeModule,
+    CoursewareModule,
+    SharedModule,
+    AppRoutingModule,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
