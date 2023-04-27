f6dc3133fcf8d4ed1d488c94b1b3019288865f10
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-27 10:26:51 -0300

    style: add mouse over feedback

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -5,7 +5,6 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   public date: string;
