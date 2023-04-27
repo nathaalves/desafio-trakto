3c7b23d7ce75db15e36f8b6f382ec832d7aeab94
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-26 21:29:57 -0300

    refactor: split courseware responsibility

--- src/app/pages/courseware/courseware.component.ts
+++ src/app/pages/courseware/courseware.component.ts
@@ -1,32 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { CoursewareService } from './courseware.service';
-import { Courseware } from './courseware.model';
-import { formatedDate } from 'src/utils/formatedDate';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-couseware',
   templateUrl: './courseware.component.html',
 })
-export class CoursewareComponent implements OnInit {
-  courseware: Courseware[] = [];
-  firstUpdatedDate = '';
-  lastUpdatedDate = '';
-
-  constructor(private coursewareService: CoursewareService) {}
-
-  ngOnInit(): void {
-    this.getCourseware();
-  }
-
-  private getCourseware() {
-    this.coursewareService.fetchData().subscribe((res) => {
-      this.courseware.push(...res.data);
-
-      const firstDate = res.data[0].updatedAt;
-      this.firstUpdatedDate = formatedDate(firstDate).substring(0, 5);
-
-      const lastDate = res.data[res.data.length - 1].updatedAt;
-      this.lastUpdatedDate = formatedDate(lastDate).substring(0, 5);
-    });
-  }
-}
+export class CoursewareComponent {}
