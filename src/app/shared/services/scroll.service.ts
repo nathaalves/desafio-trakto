45b6a1f8898f13da4e76fd03a0ca2bf3d7441538
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-27 16:57:47 -0300

    feat: create scroll service

---
+++ src/app/shared/services/scroll.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ScrollService {
+  constructor(private router: Router) {}
+
+  setupScrollTopOnNavigation() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+}
