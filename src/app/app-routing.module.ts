4763dbba29b39b7234569b6c1024ce1939201814
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-26 22:23:19 -0300

    feat: move guards to shared folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { HomeComponent } from './features/home/home.component';
-import { AuthGuard } from './guards/auth.guard';
+import { AuthGuard } from './shared/guards/auth.guard';
 import { CoursewareComponent } from './features/courseware/courseware.component';
 import { CoursewareHomeComponent } from './features/courseware/pages/courseware-home/courseware-home.component';
 import { CoursewareSeeAllComponent } from './features/courseware/pages/courseware-see-all/courseware-see-all.component';
