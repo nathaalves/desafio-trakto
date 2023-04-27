a07db046fc785e5607a7053256030b1ce2334953
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-27 11:06:36 -0300

    style: add mouse over feedback

--- src/app/features/home/components/home-anchor/home-anchor.component.ts
+++ src/app/features/home/components/home-anchor/home-anchor.component.ts
@@ -12,4 +12,5 @@ export class HomeAnchorComponent {
   @Input() borderClassColor: string = '';
   @Input() backgroundClassColor: string = '';
   @Input() link: string = '';
+  @Input() containerImageClassEffect: string = '';
 }
